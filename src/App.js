6495f882f7dac7716d3112235d010b8a6473e2de
Huynh Trung Hau <huynhhau.0703349507@gmail.com>
2022-12-08 08:42:01 +0700

    file chua han chinh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import Footer from './component/footer/Footer';
+import Home from './component/home/Home';
+import Main from './component/main/Main';
+import NavBar from './component/navbar/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+     <Home/> 
+    <Main/>
+    {/* <Footer/> */}
+    </>
   );
 }
 
